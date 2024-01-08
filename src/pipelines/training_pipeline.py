8d96bfd94071c46cc054d0255a4dec769499b117
code8Komal <shrivastavkn@rknec.edu>
2024-01-08 14:18:20 +0530

    frist commit

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,19 @@
+import os
+import sys 
+from src.logger import logging 
+from src.exception import CustomException
+import pandas as pd 
+from src.componets.data_ingestion import dataIngestion
+from src.componets.data_transformation import DataTransformation
+## from src.components.model_trainer import ModelTrainer
+
+
+if __name__=='__main__':
+    obj=dataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
+
+    data_tranformation=DataTransformation()
+
+    train_arr,test_arr,_=data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
+
